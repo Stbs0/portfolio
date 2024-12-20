93b4617cf1822a6700a683d708a798854fe3db72
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2024-12-21 00:01:42 +0300

    refractor: add resource for projects

--- constant/index.tsx
+++ constant/index.tsx
@@ -1,22 +1,75 @@
 export const Social_Icons = [
   {
-    link: "https://github.com/ibrahimmemonn",
+    link: "https://github.com/stbs0",
     image: "/Github.svg",
-    alt: "Ibrahim Memon - Social Media",
+    alt: "Mohammed Mahmoud - Social Media",
   },
   {
-    link: "https://www.instagram.com/ibii.memon/?hl=en",
+    link: "https://www.instagram.com/stbs0/?hl=en",
     image: "/Instagram.svg",
-    alt: "Ibrahim Memon - Social Media",
+    alt: "Mohammed Mahmoud - Social Media",
   },
   {
-    link: "https://www.linkedin.com/in/ibrahimmemonn/",
+    link: "https://www.linkedin.com/in/mohammed-ibrahim-mahmoud/",
     image: "/LinkedIn.svg",
-    alt: "Ibrahim Memon - Social Media",
+    alt: "Mohammed Mahmoud - Social Media",
   },
   {
     link: "https://twitter.com/Ibrahimmemonnn",
     image: "/Twitter.svg",
-    alt: "Ibrahim Memon - Social Media",
+    alt: "Mohammed Mahmoud - Social Media",
   },
+];
+export const BANNER = {
+  developer:
+    " Self-taught full-stack developer skilled in ReactJS, TypeScript, and Express, creating dynamic web solutions.",
+  pharmacist:
+    "A passionate pharmacist dedicated to helping patients find the right medication for their needs, ensuring optimal health outcomes.",
+};
+
+export const ABOUT_ME = {
+  developer: [
+    "Hi, I'm a passionate self-taught full-stack web developer with a year of dedicated learning and hands-on experience in web development. I specialize in building dynamic and responsive websites using technologies like ReactJS, TypeScript, and Express. My journey into coding started with curiosity and grew into a full-fledged passion, pushing me to create functional and visually appealing projects.",
+
+    "I'm actively seeking opportunities to bring my skills and creativity to a professional environment where I can contribute to impactful web solutions. Let's build something amazing together!",
+  ],
+  pharmacist: [""],
+};
+
+export const PROJECTS = [
+  {
+    title: "Sudan Codex",
+    url: "",
+    image: "/purpleLogo.png",
+    description:
+      "developed a website that provide FDA drug labeling information for heath care providers in Sudan, using ReactJS, TypeScript and Tailwind CSS",
+  },
+  {
+    title: "YC Directory",
+    url: "https://yc-directory-rouge-tau.vercel.app/",
+    image: "/yc_logo.png",
+    description:
+      "A website build with Next.js, TypeScript that allows users to view, add startups and companies to the YC Directory",
+  },
+  {
+    title: "Space Tourism",
+    url: "space-tourism-one-umber.vercel.app",
+    image: "/space_toursm_logo.svg",
+    description: "A website about space tourism",
+  },
+];
+
+export const SKILLS = [
+  "HTML",
+  "CSS",
+  "Tailwind CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Express",
+  "MongoDB",
+  "Git",
+  "GitHub",
 ];
