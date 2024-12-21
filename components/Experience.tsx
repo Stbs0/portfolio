6ad8ae172aae6e6d025835e9b05c82722812eedd
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2024-12-21 21:52:57 +0300

    refractor: add the pharmacist experiences

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,10 +1,58 @@
-import { Careers } from "@/app/[career]/page";
+import { type Careers } from "@/app/[career]/page";
+import { EXPERIENCES } from "@/constant";
 import { isDeveloper } from "@/lib/utils";
 import Image from "next/image";
-import React from "react";
 
-const Experience = ({career}: {career: Careers}) => {
+const Job = ({
+  job,
+  company,
+  img,
+  location,
+  startDate,
+  endDate,
+  responsibility,
+}: {
+  job: string;
+  company: string;
+  location: string;
+  startDate: string;
+  img: string;
+  endDate: string;
+  responsibility: string[];
+}) => {
+  return (
+    <div className=' container mx-auto 2xl '>
+      <div className='flex md:flex-row  gap-4 pt-5'>
+        <Image
+          src={img}
+          height={50}
+          width={60}
+          className='h-auto '
+          alt={"Mohammed Mahmoud - Pharmacy Inter"}
+        />
+        <div className='flex  items-center *:text-left flex-col '>
+          <p className={`text-black-700  `}>
+            <span className='font-semibold'>{company} </span>/ {job}
+          </p>
+          <p className='text-black-500 md:pt-0 pt-3 w-full '>
+            {startDate} - {endDate} / {location}
+          </p>
+        </div>
+      </div>
+      <ol className='list-disc dark:text-black-300 pl-20'>
+        {responsibility.map((responsibility, index) => (
+          <li
+            className='text-black-500 pt-5'
+            key={index}>
+            {responsibility}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
 
+const Experience = ({ career }: { career: Careers }) => {
   return (
     <section id='experience'>
       <h2
@@ -16,56 +64,12 @@ const Experience = ({career}: {career: Careers}) => {
       <p className=' tracking-[0.5em] text-center text-transparent font-light pb-5  bg-clip-text bg-gradient-to-r from-purple-700 to-orange-500  text-1xl '>
         EXPLORE NOW
       </p>
-      <div className=' container mx-auto 2xl '>
-        <div className='md:flex md:flex-row md:justify-between pt-5'>
-          <div className='flex items-center gap-3'>
-            <Image
-              src={"/WebHR.svg"}
-              height={30}
-              width={30}
-              alt={"Mohammed Mahmoud - UX Experience"}
-            />
-            <p className='dark:text-gray-300 '>
-              <span className='font-semibold'>WebHR /</span> Software Engineer
-            </p>
-          </div>
-          <p className='dark:text-gray-300 md:pt-0 pt-3'>
-            May 2022 - Present, United States
-          </p>
-        </div>
-
-        <p className='dark:text-gray-300 pt-5'>
-          Currently, I am working on WebHR Mobile Application, WebHR is a Cloud
-          based Social HR Software for SMEs by Verge Systems Inc. WebHR is
-          currently used in over 160 countries world wide by thousands of
-          organizations to manage HR, As a React.js developer with 2.5 year of
-          experience, I have a strong foundation in creating dynamic and
-          responsive mobile and web applications.
-          <br />
-          <br />
-          My experience with React Native has allowed me to develop
-          cross-platform mobile applications that run seamlessly on both iOS and
-          Android platforms. Additionally, my proficiency in React.js has
-          equipped me with the skills to create fast, scalable, and dynamic web
-          pages with excellent user experiences. I have a deep understanding of
-          component-based architecture and state management, and I am
-          well-versed in the latest web development trends and technologies.
-        </p>
-        <div className='flex-col flex sm:flex-row '>
-          <div className='bg-transparent  mt-5 mr-2 cursor-pointer  rounded-3xl  dark:text-white py-2 px-5  border dark:border-[#2E2E2E] w-max'>
-            React Native
-          </div>
-          <div className='bg-transparent  mt-5  mr-2 cursor-pointer  rounded-3xl  dark:text-white py-2 px-5  border dark:border-[#2E2E2E] w-max'>
-            React
-          </div>
-          <div className='bg-transparent  mt-5  mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border dark:border-[#2E2E2E] w-max'>
-            JavaScript
-          </div>
-          <div className='bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border dark:border-[#2E2E2E] w-max'>
-            Typescript
-          </div>
-        </div>
-      </div>
+      {EXPERIENCES.map((experience, index) => (
+        <Job
+          key={index}
+          {...experience}
+        />
+      ))}
     </section>
   );
 };
