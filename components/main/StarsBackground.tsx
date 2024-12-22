e560acad9501662403e6d692e2b4189503dd7ab5
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2024-12-22 09:19:56 +0300

    fix: enhance initial loading

--- components/main/StarsBackground.tsx
+++ components/main/StarsBackground.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef, Suspense } from "react";
+import React, {useRef,  useMemo } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
 // @ts-expect-error I don't care
@@ -8,32 +8,44 @@ import * as random from "maath/random/dist/maath-random.esm";
 import { useParams } from "next/navigation";
 import { isDeveloper } from "@/lib/utils";
 
+
 const StarBackground = () => {
   const ref = useRef(null);
-  const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(5000), { radius: 1.2 }),
-  );
+   const sphere = useMemo(() => {
+     return random.inSphere(new Float32Array(5000 * 3), { radius: 1.2 });
+   }, []);
   const career = useParams().career;
 
   useFrame((state, delta) => {
-    if (!ref.current) return;
-    // @ts-expect-error I don't care
-    ref.current.rotation.x -= delta / 10;
-    // @ts-expect-error I don't care
-    ref.current.rotation.y -= delta / 15;
+    if (ref.current) {
+      const time = state.clock.getElapsedTime();
+
+      // Define base rotation speed
+      const baseXSpeed = 0.1;
+      const baseYSpeed = 0.07;
+
+      // Add slight directional drift
+      const driftX = Math.sin(time * 0.1) * 0.02; // Slowly changes over time
+      const driftY = Math.cos(time * 0.1) * 0.015;
+
+      //  @ts-expect-error I don't care
+      ref.current.rotation.x += (baseXSpeed + driftX) * delta;
+      //  @ts-expect-error I don't care
+
+      ref.current.rotation.y += (baseYSpeed + driftY) * delta;
+    }
   });
   return (
-    <group rotation={[0, 0, Math.PI / 4]}>
+    <group rotation={[4, 2, Math.PI / 4]}>
       <Points
         ref={ref}
         positions={sphere}
         stride={3}
-        frustumCulled
-        >
+        frustumCulled>
         <PointMaterial
           transparent
           color={career === "developer" ? "#fff" : "#000000"}
-          size={0.005}
+          size={0.002}
           sizeAttenuation={true}
           dethWrite={false}
         />
@@ -44,7 +56,7 @@ const StarBackground = () => {
 
 const StarsCanvas = () => {
   const career = useParams().career;
-  
+
   return (
     <div
       className={`w-full h-auto fixed inset-0 ${
@@ -52,9 +64,7 @@ const StarsCanvas = () => {
       } z-[-2]
        `}>
       <Canvas camera={{ position: [0, 0, 1] }}>
-        <Suspense fallback={null}>
-          <StarBackground />
-        </Suspense>
+        <StarBackground />
       </Canvas>
     </div>
   );
